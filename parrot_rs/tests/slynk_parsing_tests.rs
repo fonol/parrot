a90b92142f8622f22c283f2de8cffd3db9d2252e
fonol <zeller@lysio.de>
2022-10-26 22:01:16 +0200

    test parsing multiple definition result

--- parrot_rs/tests/slynk_parsing_tests.rs
+++ parrot_rs/tests/slynk_parsing_tests.rs
@@ -22,8 +22,27 @@ fn parse_find_definition_result() {
 
     if let SlynkAnswer::ReturnFindDefinitionResult {  definitions, .. } = parsed {
         assert!(definitions.len()==1);
-        assert_eq!(Some(&String::from("path/to/testing.lisp")), definitions[0].file.as_ref());
+        assert_eq!(String::from("path/to/testing.lisp"), definitions[0].file);
     } else {
         panic!("Wrong enum variant")
     }
+}
+//
+// Finding the definition for "make-list" should return multiple results in SBCL code, 
+// which should be correctly parsed as a list of found definitions.
+//
+#[test]
+fn parse_find_definition_result_multiple() {
+    let parsed = SlynkAnswer::parse(r#"(:return (:ok (("(DEFUN MAKE-LIST)" (:location (:file "C:/sbcl-64/src/code/list.lisp") (:position 1) (:snippet "(defun MAKE-LIST "))) ("(:DEFINE-SOURCE-TRANSFORM MAKE-LIST)" (:location (:file "C:/sbcl-64/src/compiler/srctran.lisp") (:position 1) nil)) ("(DECLAIM MAKE-LIST
+        SB-C:DEFKNOWN)" (:location (:file "C:/sbcl-64/src/compiler/fndb.lisp") (:position 1) nil)))) 3)"#);
+    assert!(matches!(parsed, SlynkAnswer::ReturnFindDefinitionResult { .. }));
+    if let SlynkAnswer::ReturnFindDefinitionResult {  definitions, .. } = parsed {
+        assert!(definitions.len()==3);
+        assert_eq!(String::from("C:/sbcl-64/src/code/list.lisp"), definitions[0].file);
+        assert_eq!(String::from("C:/sbcl-64/src/compiler/srctran.lisp"), definitions[1].file);
+        assert_eq!(String::from("C:/sbcl-64/src/compiler/fndb.lisp"), definitions[2].file);
+    } else {
+        panic!("Wrong enum variant")
+    }
+
 }
