d84f8e0853f84078151db56982e5bccf8b4785dd
fonol <zeller@lysio.de>
2022-10-27 12:53:41 +0200

    add eventbus for easier component communication

---
+++ parrot_ui/scripts/eventbus.js
@@ -0,0 +1,27 @@
+class EventBus {
+    constructor(){
+        this.events = {};
+    }
+    on(eventName, fn) {
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(fn);
+    }
+    off(eventName, fn) {
+        if (this.events[eventName]) {
+            for (var i = 0; i < this.events[eventName].length; i++) {
+                if (this.events[eventName][i] === fn) {
+                    this.events[eventName].splice(i, 1);
+                    break;
+                }
+            };
+        }
+    }
+    trigger(eventName, data) {
+        if (this.events[eventName]) {
+            for (let cb of this.events[eventName]) {
+                cb(data);
+            }
+        }
+    }
+}
+window.$bus = new EventBus();
