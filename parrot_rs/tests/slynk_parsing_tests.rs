faa6fcffa988d8c01f8c1c6366392858076d8bc2
fonol <zeller@lysio.de>
2022-10-31 12:22:21 +0100

    add a test for write-values parsing

--- parrot_rs/tests/slynk_parsing_tests.rs
+++ parrot_rs/tests/slynk_parsing_tests.rs
@@ -1,6 +1,6 @@
 use std::{assert};
 
-use parrot_rs::{self, repl::SlynkAnswer};
+use parrot_rs::{self, repl::{SlynkAnswer, ChannelMethod}};
 
 #[test]
 fn parse_compilation_result_failed() {
@@ -45,4 +45,34 @@ fn parse_find_definition_result_multiple() {
         panic!("Wrong enum variant")
     }
 
+}
+
+///
+/// This ist the returned value from slynk when evaluating (list-all-packages) in the REPL
+#[test]
+fn parse_write_values_list_all_packages() {
+ let parsed = SlynkAnswer::parse(r#"(:channel-send 1 (:write-values (("(#<PACKAGE \"SLYNK-MATCH\"> #<PACKAGE \"SLYNK-TRACE-DIALOG\">         #<PACKAGE \"SB-ALIEN-INTERNALS\"> #<PACKAGE \"COMMON-LISP-USER\"> #<PACKAGE \"SB-DEBUG\"> #<PACKAGE \"SLYNK-BACKEND\"> #<PACKAGE \"SLYNK-SOURCE-PATH-PARSER\"> #<PACKAGE \"SLYNK-APROPOS\">
+ #<PACKAGE \"COMMON-LISP\"> #<PACKAGE \"SB-DI\"> #<PACKAGE \"SB-CLTL2\">
+ #<PACKAGE \"SB-WALKER\"> #<PACKAGE \"SB-PCL\"> #<PACKAGE \"SB-APROF\">
+ #<PACKAGE \"SB-IMPL\"> #<PACKAGE \"SB-VM\"> #<PACKAGE \"SB-FORMAT\">
+ #<PACKAGE \"SB-POSIX\"> #<PACKAGE \"SB-PRETTY\"> #<PACKAGE \"SB-LOCKLESS\">
+ #<PACKAGE \"SB-ASSEM\"> #<PACKAGE \"SB-EVAL\"> #<PACKAGE \"SLYNK\">
+ #<PACKAGE \"SLYNK-SOURCE-FILE-CACHE\"> #<PACKAGE \"SB-INTROSPECT\">
+ #<PACKAGE \"SB-X86-64-ASM\"> #<PACKAGE \"SB-UNICODE\"> #<PACKAGE \"SLYNK-API\">
+ #<PACKAGE \"SB-EXT\"> #<PACKAGE \"SLYNK-MOP\"> #<PACKAGE \"SB-SEQUENCE\">
+ #<PACKAGE \"SLYNK-LOADER\"> #<PACKAGE \"SB-THREAD\"> #<PACKAGE \"SB-BSD-SOCKETS\">
+ #<PACKAGE \"SB-FASL\"> #<PACKAGE \"SLYNK-IO-PACKAGE\"> #<PACKAGE \"SB-PROFILE\">
+ #<PACKAGE \"SB-DISASSEM\"> #<PACKAGE \"SB-MOP\"> #<PACKAGE \"SLYNK-RPC\">
+ #<PACKAGE \"SB-KERNEL\"> #<PACKAGE \"SB-WIN32\"> #<PACKAGE \"SB-INT\">
+ #<PACKAGE \"SB-BSD-SOCKETS-INTERNAL\"> #<PACKAGE \"SB-SYS\">
+ #<PACKAGE \"SLYNK-COMPLETION\"> #<PACKAGE \"SB-LOOP\"> #<PACKAGE \"SB-GRAY\"> #<PACKAGE \"SLYNK-GRAY\"> #<PACKAGE \"SB-C\"> #<PACKAGE \"SLYNK-STICKERS\">
+ #<PACKAGE \"KEYWORD\"> #<PACKAGE \"SB-UNIX\"> #<PACKAGE \"SB-BIGNUM\">
+ #<PACKAGE \"SLYNK-SBCL\"> #<PACKAGE \"SB-BROTHERTREE\">
+ #<PACKAGE \"SLYNK-COMPLETION-LOCAL-NICKNAMES-TEST\"> #<PACKAGE \"SLYNK-MREPL\"> #<PACKAGE \"SB-REGALLOC\"> #<PACKAGE \"SB-ALIEN\">)" 0 nil))))"#);
+assert!(matches!(parsed, SlynkAnswer::ChannelSend { .. }));
+if let SlynkAnswer::ChannelSend {  method, .. } = parsed {
+    assert!(matches!(method, ChannelMethod::WriteValues(..)));
+} else {
+    panic!("Wrong enum variant")
+}
 }
